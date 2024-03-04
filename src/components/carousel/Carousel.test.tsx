182a9fe8d5d41a642a2e834131864215ab30db3a
Junihor Moran <newiclus2005@gmail.com>
2024-03-04 17:25:25 -0500

    Change: add test cases to Carousel unit-test

--- src/components/carousel/Carousel.test.tsx
+++ src/components/carousel/Carousel.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 
 import Carousel from "./index";
 
@@ -7,8 +7,11 @@ describe("Carousel component test", () => {
   it("Should be render a <Carousel /> component", () => {
     render(
       <Carousel nodeId="main-carousel">
-        <h2>Carousel</h2>
-      </Carousel>,
+        <Carousel.Item
+          sideContent="Slide 1"
+          image="https://fastly.picsum.photos/id/9/900/600"
+        />
+      </Carousel>
     );
 
     expect(screen.getByTestId("main-carousel")).toBeTruthy();
@@ -27,7 +30,7 @@ describe("Carousel component test", () => {
           sideContent="Slide 2"
           image="https://fastly.picsum.photos/id/12/900/600"
         />
-      </Carousel>,
+      </Carousel>
     );
 
     expect(screen.getByText("Slide 1")).toBeTruthy();
@@ -38,7 +41,7 @@ describe("Carousel component test", () => {
     render(
       <Carousel nodeId="main-carousel">
         <Carousel.Item sideContent="Slide 1" image="" />
-      </Carousel>,
+      </Carousel>
     );
 
     expect(screen.queryByRole("img")).toBeNull();
@@ -47,10 +50,61 @@ describe("Carousel component test", () => {
   it("Should be render a <Carousel /> component with custom classes", () => {
     render(
       <Carousel nodeId="main-carousel" className="custom-class">
-        <h2>Carousel</h2>
-      </Carousel>,
+        <Carousel.Item
+          sideContent="Slide 1"
+          image="https://fastly.picsum.photos/id/9/900/600"
+        />
+      </Carousel>
     );
 
     expect(screen.getByTestId("main-carousel")).toHaveClass("custom-class");
+  });
+
+  it("Should be render a <Carousel /> component with dots buttons", () => {
+    const settings = {
+      dots: true,
+    };
+
+    render(
+      <Carousel nodeId="main-carousel" settings={settings}>
+        <Carousel.Item
+          sideContent="Slide 1"
+          image="https://fastly.picsum.photos/id/9/900/600"
+        />
+        <Carousel.Item
+          sideContent="Slide 2"
+          image="https://fastly.picsum.photos/id/12/900/600"
+        />
+      </Carousel>
+    );
+
+    expect(screen.getByRole("dot", { name: "Go to slide 1" })).toBeTruthy();
+    expect(screen.getByRole("dot", { name: "Go to slide 2" })).toBeTruthy();
+  });
+
+  it("<Carousel /> should be displace slider when the user click on Next/Prev buttons", async () => {
+    render(
+      <Carousel nodeId="main-carousel">
+        <Carousel.Item
+          key="slide-1"
+          sideContent="Slide 1"
+          image="https://fastly.picsum.photos/id/9/900/600"
+        />
+        <Carousel.Item
+          key="slide-2"
+          sideContent="Slide 2"
+          image="https://fastly.picsum.photos/id/12/900/600"
+        />
+      </Carousel>
+    );
+
+    // const nextButton = screen.getByRole("nextbutton");
+    // const prevButton = screen.getByRole("prevbutton");
+    const slides = screen.getAllByTestId("carousel-slideItem");
+
+    waitFor(() => {
+      expect(slides[0]).toHaveAttribute("aria-hidden", "false");
+      expect(slides[1]).toHaveAttribute("aria-hidden", "false");
+    });
   });
 });
